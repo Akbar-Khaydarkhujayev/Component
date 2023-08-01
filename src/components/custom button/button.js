a87701250f529fff10e289b012cc23f43a17d3b1
Akbar <akbarboss91@gmail.com>
2023-08-01 17:37:55 +0500

    Custom Button

--- src/components/custom button/button.js
+++ src/components/custom button/button.js
@@ -3,6 +3,8 @@ import React from 'react';
 function Button(props) {
     return (
         <div className="button-wrapper">
+            <div className="button-title">Menu</div>
+            <div className="button-icon"><i className="fa-solid fa-house button-icon1"></i></div>
             <div className="line line1"></div>
             <div className="line line2"></div>
             <div className="line line3"></div>

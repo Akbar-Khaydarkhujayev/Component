41f5d12d565f206d5e9805bb69d6621835dbe092
Akbar <akbarboss91@gmail.com>
2023-08-02 13:46:17 +0500

    Round Bar

--- src/components/circular chart/round-chart.js
+++ src/components/circular chart/round-chart.js
@@ -1,17 +1,13 @@
-import React, {useEffect} from 'react';
-import CountUp from "react-countup";
+import React, {useEffect, useRef, useState} from 'react';
 
 function RoundChart(props) {
 
-    useEffect(() => {
-        document.getElementById('rott').classList.add("qwer");
-    }, [])
-
     return (
         <div className="round-parent">
-            <span className="value"><CountUp start={0} end={props.data}></CountUp> Cars</span>
-            <div className="round" id='rott'
-                 style={{background: `conic-gradient(#22D8E3 ${props.data-2}deg, transparent ${props.data+2}deg`}}></div>
+            <span className="value">{props.data} %</span>
+            <div className="round" style={{background: `conic-gradient(#22D8E3 ${props.data-2}deg, transparent ${props.data+2}deg)`}}></div>
+            <div className="round-underlay"></div>
+            <img src="https://cdn-icons-png.flaticon.com/512/6015/6015853.png" alt="#" className="round-img"/>
         </div>
     );
 }
